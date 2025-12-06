bf56262abd4927413cf219d39b7cc02527a8aca9
Michael Nguyen <michaelalexnguyen@gmail.com>
2025-12-05 23:06:11 -0500

    day 5

--- 2025/05/05.py
+++ 2025/05/05.py
@@ -4,13 +4,38 @@ from utils import Solution, read_data
 class Day05Solution(Solution):
     def __init__(self):
         data = read_data("05")
-        self.data = data
+        ranges, ingredients = data.split("\n\n")
+        ranges = ranges.split("\n")
+        self.ranges = [
+            list([int(line.split("-")[0]), int(line.split("-")[1])]) for line in ranges
+        ]
+        self.ingredients = list(filter(None, ingredients.split("\n")))
+        print(self.ranges)
+        print(self.ingredients)
 
     def part1(self):
-        pass
+        acc = 0
+        for ingredient in self.ingredients:
+            for range_start, range_end in self.ranges:
+                if range_start <= int(ingredient) <= range_end:
+                    acc += 1
+                    break
+
+        print("part 1: ", acc)
 
     def part2(self):
-        pass
+        ranges = []
+        for range_start, range_end in sorted(self.ranges):
+            if not ranges or ranges[-1][1] < range_start:
+                ranges.append([range_start, range_end])
+            else:
+                ranges[-1][1] = max(ranges[-1][1], range_end)
+
+        acc = 0
+        for range_start, range_end in ranges:
+            acc += range_end - range_start + 1
+
+        print("part 2: ", acc)
 
 
 solution = Day05Solution()
